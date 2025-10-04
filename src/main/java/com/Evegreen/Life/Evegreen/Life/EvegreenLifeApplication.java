325a9cb14b35af914d5253232f3b48b9393ff2ed
devpa <devparth2021@gmail.com>
2025-10-04 13:48:54 +0100

    Initial commit

---
+++ src/main/java/com/Evegreen/Life/Evegreen/Life/EvegreenLifeApplication.java
@@ -0,0 +1,13 @@
+package com.Evegreen.Life.Evegreen.Life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvegreenLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvegreenLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Evegreen/Life/Evegreen/Life/EvegreenLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Evegreen.Life.Evegreen.Life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvegreenLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
